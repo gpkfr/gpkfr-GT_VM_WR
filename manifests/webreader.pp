2c15b97faecf566b5a87c94f884396bcffb5c05c
Guillaume Pancak <guillaume@MacBook-Pro-de-Guillaume.local>
2014-06-06 12:39:03 +0200

    updated webreader module

--- manifests/webreader.pp
+++ manifests/webreader.pp
@@ -1,47 +1,14 @@
-/*Exec["apt-update"] -> Package <| |>
+class { 'webreader': }
 
-exec { "apt-update":
-    command => "/usr/bin/apt-get update",
-}
-
-$nginx = "nginx-light"
-$base = [ "htop", "pydf", "screen", $nginx ]
-
-#Configure dotdeb repo.
-include apt
-
-apt::source { 'dotdeb':
-    location   => 'http://packages.dotdeb.org',
-    release    => 'wheezy-php55',
-    repos      => 'all',
-    key        => '89DF5277',
-    key_source => 'http://www.dotdeb.org/dotdeb.gpg',
- }->package { $base:
-	  ensure   => 'latest',
-    require =>  Exec [ 'apt-update'] 	
- }
-
-
-file { '/home/vagrant/.screenrc':
-	source	=> '/vagrant/files/screenrc',
-	owner	=> 'vagrant',
-	group	=> 'vagrant',
-	mode	=> '644',
-	require => Package['screen'],
-}
-
-file { '/home/vagrant/.bash_aliases':
-  source => '/vagrant/files/bash_aliases',
-  owner  => 'vagrant',
-  group  => 'vagrant',
-  mode   => '644',
-}
-*/
-
-class { 'webreader':
-  script_name => 'webreader',
+webreader::vhost { 'test.local':
+  script_name => 'wr',
   wruser      => 'vagrant',
   wrgrp       => 'vagrant',
-  server_name => 'test.local',
+  nodeapp_dir => '/var/www/wr/dist'
 }
+
+#  script_name => 'wr',
+#  wruser      => 'vagrant',
+#  wrgrp       => 'vagrant',
+#  server_name => 'test.local',
 
